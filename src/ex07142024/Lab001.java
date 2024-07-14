febed134f24fa01a83c09426319898413ddbcfd1
Keerti Kerur <keertikerur1195@gmail.com>
2024-07-14 19:12:22 +0530

    Commit code changes

--- src/ex07142024/Lab001.java
+++ src/ex07142024/Lab001.java
@@ -3,5 +3,8 @@ package ex07142024;
 public class Lab001 {
     public static void main(String[] args) {
         System.out.println("Hello Git Hub");
+        System.out.println("Commiting the code changes");
+
+
     }
 }
